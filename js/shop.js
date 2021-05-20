5689082eaa74cd1b8d6fceac0fb80e4aaef33b5c
lkp25 <lkp25@o2.pl>
2021-05-20 15:49:41 +0200

    added shop products rendering from json database

--- js/shop.js
+++ js/shop.js
@@ -1,8 +1,36 @@
+const productTemplate = document.querySelector('#product-template')
+const productsGrid = document.querySelector('.products-grid')
+
+
+
 //get products from json database
 async function getProducts(){
     const fetchProducts = await fetch('./products.json')
     const products = await fetchProducts.json()
 
     console.log(products);
+
+    renderProducts(products)
+   
 }
 getProducts()
+
+
+
+function renderProducts(products){
+    products.forEach(product => {
+        // get template
+        const template = productTemplate.content.cloneNode(true)
+        console.log(template);
+        template.querySelector('img').setAttribute('src', `./img/img-large/${product.image}`)
+        template.querySelector('img').setAttribute('alt', `${product.name}`)
+        template.querySelector('.product-name').innerText = `${product.name}`
+        template.querySelector('.product-category').innerText = `${product.category}`
+        template.querySelector('.product-description').innerText = `${product.description}`
+        template.querySelector('.product-price').innerText = `${product.priceInCents}`
+        
+        
+        productsGrid.appendChild(template)
+
+    });
+}
