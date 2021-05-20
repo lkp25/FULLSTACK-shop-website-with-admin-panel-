f836c35a7b3c6cce4a3c43630404f0f5e359a430
lkp25 <lkp25@o2.pl>
2021-05-20 16:00:58 +0200

    basic add to cart btn logi

--- js/shop.js
+++ js/shop.js
@@ -1,7 +1,9 @@
 const productTemplate = document.querySelector('#product-template')
 const productsGrid = document.querySelector('.products-grid')
 
-
+productsGrid.addEventListener('click', (e)=>{
+    console.log(e.target.classList.contains('product-add-to-cart-btn'));
+})
 
 //get products from json database
 async function getProducts(){
@@ -21,7 +23,11 @@ function renderProducts(products){
     products.forEach(product => {
         // get template
         const template = productTemplate.content.cloneNode(true)
-        console.log(template);
+        
+        //give the id to card product
+        template.querySelector('.product-card').dataset.id = `${product.id}`
+        
+        //fill all data
         template.querySelector('img').setAttribute('src', `./img/img-large/${product.image}`)
         template.querySelector('img').setAttribute('alt', `${product.name}`)
         template.querySelector('.product-name').innerText = `${product.name}`
@@ -29,7 +35,9 @@ function renderProducts(products){
         template.querySelector('.product-description').innerText = `${product.description}`
         template.querySelector('.product-price').innerText = `${product.priceInCents}`
         
-        
+        //add to cart button
+
+        //add product to all products
         productsGrid.appendChild(template)
 
     });
