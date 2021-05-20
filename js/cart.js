b0f27cac9451a58a80bdc138f1e6f69a2f2fee38
lkp25 <lkp25@o2.pl>
2021-05-20 19:12:01 +0200

    cart stored in session storage

--- js/cart.js
+++ js/cart.js
@@ -8,11 +8,13 @@ productsGrid.addEventListener('click', (e)=>{
      const product = e.target.closest('.product-card')
     
      
-        
+        //check if item is already in the cart
         const isAlreadyOrdered = itemsInCart.find(item => item.id === product.dataset.id)
-            // item.quantity++
+            
         if(isAlreadyOrdered){
             isAlreadyOrdered.quantity++
+
+        //if it is new, create new entry in itemsInCart
         }else{
             const newOrder = {
                 id: product.dataset.id,
@@ -24,15 +26,20 @@ productsGrid.addEventListener('click', (e)=>{
             
             itemsInCart.push(newOrder)
         }
-            
+       //save shopping cart in session storage 
+       sessionStorage.setItem("cart", JSON.stringify(itemsInCart))  
        
-
-     
-     
+       
     }
  })
 
+//cart icon is clicked - open/close cart
 cartIcon.addEventListener('click', ()=>{
     cartContent.classList.toggle('show-cart')
     
+    //render items if cart is open
+    if(cartContent.classList.contains('show-cart')){
+        const productsToRender = JSON.parse(sessionStorage.getItem("cart")) 
+        console.log(productsToRender);
+    }
 })
