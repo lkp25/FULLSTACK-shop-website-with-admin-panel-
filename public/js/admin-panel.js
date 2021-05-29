d21093dbb33f7580a8b8e1ad9f5940636819a5a8
lkp25 <lkp25@o2.pl>
2021-05-29 22:12:23 +0200

    add new product logic complete

--- public/js/admin-panel.js
+++ public/js/admin-panel.js
@@ -1,4 +1,5 @@
 const prodList = document.getElementById('current-products')
+const addNewProductTable = document.getElementById('add-new-product-table')
 const singleItemTemplate = document.getElementById('single-product-table-entry')
 
 
@@ -87,6 +88,13 @@ document.addEventListener('click', (e)=>{
         //render new view
         renderProducts()
     }
+    if(e.target.classList.contains('add-item-btn')){
+        addNewItem()
+        //remove old view
+        removeAllProductsFromView()
+        //render new view
+        renderProducts()
+    }
 })
 
 
@@ -145,4 +153,37 @@ function displayConfirmationWindow(){
     const saveChangesBtn = document.querySelector('.save-changes-btn')
 
     saveChangesBtn.parentElement.appendChild(confirmationWindow)
+}
+
+function addNewItem(){
+    const allInputs = Array.from(addNewProductTable.querySelectorAll('input'))     
+
+    const name = addNewProductTable.querySelector('.add-item-name').children[0].value
+    const price = addNewProductTable.querySelector('.add-item-price').children[0].value
+    const description = addNewProductTable.querySelector('.add-item-description').children[0].value
+    const category = addNewProductTable.querySelector('.add-item-category').children[0].value
+    const img = addNewProductTable.querySelector('.add-item-img').children[0].value
+    
+    allInputs.find(field => {
+        //stop if any field is empty
+        if(!field.value){
+            
+            field.placeholder = "REQUIRED!"
+            return
+        }
+        //all filled - add new product
+        const newProduct = {
+            id: Math.floor(new Date().getFullYear() * new Date().getDate() * Math.random() * 100000),
+               name: name,
+               priceInCents: price,
+               image: img,
+               description: description,
+               category: category
+        }
+    
+        allProducts.push(newProduct)
+        allInputs.forEach(field => field.value ='')
+    })
+
+    
 }
