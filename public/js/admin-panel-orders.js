7f0112314973db79e2409a35e73974738fcce4f7
lkp25 <lkp25@o2.pl>
2021-05-31 12:57:41 +0200

    rendering orders from db to admin panel orders logic added plus styling

--- public/js/admin-panel-orders.js
+++ public/js/admin-panel-orders.js
@@ -1,4 +1,8 @@
 let allOrders = []
+const singleOrderTemplate = document.getElementById('db-order-template')
+const allOrdersList = document.getElementById('db-all-orders')
+const root = document.documentElement
+
 
 async function getProducts(){
 
@@ -7,6 +11,8 @@ async function getProducts(){
 
     console.log(orders);
     allOrders = orders
+    
+    //immidiately parse strigified object from db:
     allOrders.forEach(element => {
         element.orderData = JSON.parse(element.orderData)
         
@@ -14,3 +20,35 @@ async function getProducts(){
    
 }
 getProducts()
+
+
+function renderAllOrders(sort = 'date'){
+    allOrders.forEach(item => {
+        const template = singleOrderTemplate.content.cloneNode(true)
+        console.log(template);
+        template.querySelector('.db-order-surname').textContent = item.orderData.surname
+        allOrdersList.appendChild(template)
+    })
+    
+}
+renderAllOrders()
+
+
+//sent-checkbox
+// const checkbox = document.querySelector('.checkbox')
+document.addEventListener('click', (e)=>{
+    if(e.target.classList.contains('checkbox')){
+        const checkbox = e.target
+        if(checkbox.dataset.sent === 'false'){
+            checkbox.dataset.sent = 'true'
+            checkbox.classList.add('vis')
+        }
+        else{
+            checkbox.dataset.sent = 'false'
+            checkbox.classList.remove('vis')
+        }
+    }
+    
+    
+   
+})
