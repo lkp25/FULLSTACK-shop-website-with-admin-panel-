1b1153b4311029547e4e534346c78a64a3a18a09
lkp25 <lkp25@o2.pl>
2021-06-10 17:52:18 +0200

    writing changes in products tab to the main products.json file - whens saved, changes are immidiately visible in the shop

--- routes/admin.js
+++ routes/admin.js
@@ -17,59 +17,60 @@ const sqlConnection = mysql.createConnection({
   database: process.env.DB_NAME
 })
 sqlConnection.connect(function(err) {
-    if (err) throw err;
-   
+  if (err) throw err;
+  
 });
-//serve HTML:
-router.get('/admin-products', (req, res, next) =>{
-    console.log('welcome to admin page - products');
-    res.sendFile(path.join(rootDir, 'views', 'admin-panel-products.html'))
-})
-router.get('/admin-orders', (req, res, next) =>{
-    console.log('welcome to admin page - orders');
-    res.sendFile(path.join(rootDir, 'views', 'admin-panel-orders.html'))
-})
 
 
+////////ROUTES FROM ORDERS TAB////////
 //admin route for getting all orders
 router.get('/getorders', (req, res, next) =>{
-    sqlConnection.query("SELECT * FROM orders", function (err, result, fields) {
-      if (err) throw err;
-      console.log('HERE ARE ALL ORDERS FROM DB:' , result);
-      res.send(result)
-    });
-    
+  sqlConnection.query("SELECT * FROM orders", function (err, result, fields) {
+    if (err) throw err;
+    console.log('HERE ARE ALL ORDERS FROM DB:' , result);
+    res.send(result)
+  });
+  
 })
 
 //update single order
 router.post('/update-order-status',  (req, res, next) =>{
-       
+  
   //headers first!!
   // res.header({"Content-Type": "application/json"})
   const requestBody =  req.body
   
   //echo back the request + send response message
   res.json({
-      requestBody: req.body,
-      responseMessage: 'order status updated!'
+    requestBody: req.body,
+    responseMessage: 'order status updated!'
   })
   //save order in the database
   console.log(requestBody);
   sqlConnection.query(`UPDATE orders SET orderData='${JSON.stringify(requestBody.orderData) }' WHERE id=${requestBody.id}`, function (err, result, fields) {
-      if (err) throw err;
-      console.log(result);
-      // res.send(result)
-    });
+    if (err) throw err;
+    console.log(result);
+    // res.send(result)
+  });
   
   //write new file for order to JSONdatabase 
 })
 
-////////PRODUCTS////////
+////////ROUTES FROM PRODUCTS TAB////////
 router.post('/update-products-in-offer', (req, res, next)=>{
-  writeFile(`jsonDatabase/nownownown.json`, JSON.stringify(req.body), err =>  console.log(err))
-  res.status(201).send()
+  writeFile(`public/products-json/products.json`, JSON.stringify(req.body), err =>  console.log(err))
+  res.status(201).send(req.body)
 })
 
+//serve HTML:
+router.get('/admin-products', (req, res, next) =>{
+  console.log('welcome to admin page - products');
+  res.sendFile(path.join(rootDir, 'views', 'admin-panel-products.html'))
+})
+router.get('/admin-orders', (req, res, next) =>{
+  console.log('welcome to admin page - orders');
+  res.sendFile(path.join(rootDir, 'views', 'admin-panel-orders.html'))
+})
 
 
 module.exports = router
