ad39b7ccf328a34e51e3c51294b6cdae2615e5fa
lkp25 <lkp25@o2.pl>
2021-06-28 16:36:54 +0200

    questions table styled and armed with all needed datasets

--- public/js/admin-panel-questions.js
+++ public/js/admin-panel-questions.js
@@ -8,15 +8,35 @@ async function getAllQuestionsFromDB(){
     const n = await fetch('http://localhost:5000/get-all-questions')
     const nww = await n.json()
     allQuestions = nww
+    allQuestions.forEach(question => question.value = JSON.parse(question.value))
+    console.log(allQuestions);
 
     renderAllQuestions(allQuestions)
 }
 
 function renderAllQuestions(allQuestions){
-    allQuestions.forEach(question =>{
+    //newest at the top
+    allQuestions.reverse()
+
+    allQuestions.forEach((question, index) =>{
        const template = document.getElementById('db-questions-template').content.cloneNode(true)
        
-       template.querySelector('.db-question-important').textContent = question.important? "YES" : "NO"
+       const info = question.value
+       template.children[0].dataset.id = question.id
+       template.children[0].dataset.index = index
+    //    template.dataset.allData = question
+       
+    template.querySelector('.db-question-content').textContent = info.text 
+    template.querySelector('.db-question-date').textContent = info.date 
+    template.querySelector('.db-question-email').textContent = info.email 
+    template.querySelector('.db-question-email').dataset.email = info.email
+
+    template.querySelector('.db-question-remove').dataset.index = index 
+    template.querySelector('.db-question-remove').dataset.id = question.id
        questionsList.appendChild(template)
     })
+}
+
+function clearAllQuestions(){
+    questionsList.querySelectorAll('.db-single-question').forEach(element => element.remove())
 }
