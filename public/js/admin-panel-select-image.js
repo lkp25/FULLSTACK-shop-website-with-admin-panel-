7894fff27556e4d4791f3bb39b8d4cfbaaa9e27c
lkp25 <lkp25@o2.pl>
2021-06-30 17:37:51 +0200

    img selection list popupulated with real names from images stored on server

--- public/js/admin-panel-select-image.js
+++ public/js/admin-panel-select-image.js
@@ -1,4 +1,6 @@
-
+const selectImgModal = document.querySelector('.select-image-modal-overlay')
+const imagesListElement = document.querySelector('.images-list')
+hideImageChooseModal()
 
 document.addEventListener('click', e=>{
     if(
@@ -14,17 +16,40 @@ document.addEventListener('click', e=>{
     getImageFilesList()
 
   }
+  //CLOSE MODAL - overlay clicked
+  if(e.target.classList.contains('select-image-modal-overlay')){
+    hideImageChooseModal()
+  }
+  //CONFIRM BUTTON
+  if(e.target.classList.contains('select-image-modal-confirm-btn')){
+    hideImageChooseModal()
+  }
 })
+
+
 function showImageChooseModal(){
-    const modalContainer = document.createElement('div')
-    modalContainer.classList.add('select-image-modal-container')
-    document.body.appendChild(modalContainer)
+  selectImgModal.style.display = null
+    
+}
+function hideImageChooseModal(){
+  selectImgModal.style.display = 'none'
 }
 
 async function getImageFilesList(){
     const request = await fetch('http://localhost:5000/list-of-image-files')
     const imagesList = await request.json()
-    console.log(imagesList);
+    
+    // remove all old images
+    Array.from(imagesListElement.children).forEach(child => child.remove() )
+     
+    imagesList.forEach(item => {
+      const listElement = document.createElement('li')
+      listElement.textContent = item
+      listElement.dataset.filename = item
+
+      imagesListElement.appendChild(listElement)
+    })
+
 }
 
 // template.querySelector('img').setAttribute('src', `./img/img-large/${product.image}`)
