c321ab6fc050dcbdbf08e15589ce051d2426169e
lkp25 <lkp25@o2.pl>
2021-07-02 19:08:29 +0200

    new img files uploaded correctly to the desired directory

--- routes/admin-products.js
+++ routes/admin-products.js
@@ -1,20 +1,21 @@
 const path = require('path');
 const fs = require('fs');
+
+//multer file upload
 const multer = require('multer')
 const storage = multer.diskStorage({
   destination: (req, file, cb)=> {
-    cb(null, "uploads")
+    cb(null, "public/img/img-large")
   },
   filename: (req, file, cb)=> {
     const {originalname} = file
     cb(null, originalname)
   }
 })
-
 const upload = multer({ storage });
 
-const express = require('express');
 
+const express = require('express');
 const router = express.Router()
 
 const rootDir = require('../util/path');
@@ -22,6 +23,7 @@ const { writeFile } = require('fs');
 
 require('dotenv').config()
 
+//mySQL
 const mysql = require('mysql')
 const sqlConnection = mysql.createConnection({
   host: process.env.PORT,
