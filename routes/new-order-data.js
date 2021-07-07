26938b5a4a0201ae099f2407ab8cb2eade2e84d5
lkp25 <lkp25@o2.pl>
2021-07-07 12:44:58 +0200

    new-order-data routes set torun through mysql2 pool

--- routes/new-order-data.js
+++ routes/new-order-data.js
@@ -3,39 +3,23 @@ const path = require('path');
 const express = require('express');
 
 const router = express.Router()
+const db = require('../util/mySQLdb')
 
 require('dotenv').config()
 
-const mysql = require('mysql')
-const sqlConnection = mysql.createConnection({
-  host: process.env.PORT,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASS,
-  database: process.env.DB_NAME
-})
-sqlConnection.connect(function(err) {
-    if (err) throw err;
-   
-});
 
 router.post('/save-order', async (req, res, next) =>{
            
     const requestBody = await req.body
+    db.execute(`INSERT INTO orders(orderData) VALUES('${JSON.stringify(requestBody)}')`)
+    .then(([recordsArray, fieldsDataArray] )=>{
+      res.send({
+        requestBody: req.body,
+        responseMessage: 'your order was successfuly created'
+    })
+    })
+    .catch((error)=>{console.log(error);}) 
     
-    //echo back the request + send response message
-   
-    //save order in the database
-    sqlConnection.query(`INSERT INTO orders(orderData) VALUES('${JSON.stringify(requestBody)}')`, function (err, result, fields) {
-        if (err) throw err;
-        console.log(result);
-        res.send({
-          requestBody: req.body,
-          responseMessage: 'your order was successfuly created'
-      })
-      });
-    
-    //write new file for order to JSONdatabase 
-    // writeFile(`jsonDatabase/${req.body.surname}-${req.body.orderId}.json`, JSON.stringify(req.body), err =>  console.log(err))
 })
 
 
