59aea7133f4490e55295670ee968eb3c4fd790b2
lkp25 <lkp25@o2.pl>
2021-07-07 12:06:49 +0200

    separate js file for bubbles animation

--- routes/admin-questions.js
+++ routes/admin-questions.js
@@ -1,34 +1,37 @@
 const path = require('path');
+const db = require('../util/mySQLdb')
 
 const express = require('express');
-
 const router = express.Router()
-
 require('dotenv').config()
 
-const mysql = require('mysql')
-const sqlConnection = mysql.createConnection({
-  host: process.env.PORT,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASS,
-  database: process.env.DB_NAME
-})
-sqlConnection.connect(function(err) {
-    if (err) throw err;
+// const mysql = require('mysql')
+// const sqlConnection = mysql.createConnection({
+//   host: process.env.PORT,
+//   user: process.env.DB_USER,
+//   password: process.env.DB_PASS,
+//   database: process.env.DB_NAME
+// })
+// sqlConnection.connect(function(err) {
+//     if (err) throw err;
    
-});
+// });
 
 
 //save new customer question 
 router.post('/new-customer-question', async (req, res, next) =>{
-    console.log(req.body);
-
+  db.execute(`INSERT INTO questions(value) VALUES('${JSON.stringify(req.body)}')`)
+  .then(([recordsArray, fieldsDataArray] )=>{
     
-    sqlConnection.query(`INSERT INTO questions(value) VALUES('${JSON.stringify(req.body)}')`, function (err, result, fields) {
-        if (err) throw err;
-        console.log(result);
-        res.send(result)
-      });
+    res.send(recordsArray)
+  })
+  .catch((error)=>{console.log(error);})
+    
+    // sqlConnection.query(`INSERT INTO questions(value) VALUES('${JSON.stringify(req.body)}')`, function (err, result, fields) {
+    //     if (err) throw err;
+    //     console.log(result);
+    //     res.send(result)
+    //   });
 
     
 })
@@ -55,7 +58,7 @@ router.get('/delete-question', async (req, res, next) =>{
 // })
 
 
-const db = require('../util/mySQLdb')
+
 //for admin panel display all questions:
 router.get('/get-all-questions', (req, res, next) =>{
   db.execute('SELECT * FROM questions')
