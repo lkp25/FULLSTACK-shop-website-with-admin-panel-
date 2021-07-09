68c0ab7e138bcb29f5bbcedfa2f67dc69b0f891a
lkp25 <lkp25@o2.pl>
2021-07-09 11:58:45 +0200

    individual prod page tmplate populating with proper data functionality added

--- public/js/individual-product-page.js
+++ public/js/individual-product-page.js
@@ -15,10 +15,16 @@ async function openNewProductPage(prodId){
     window.location.href = response.url
     sessionStorage.setItem('current-product-id', prodId)
 }
-
+getItemData()
 function getItemData(){
     const itemData = JSON.parse(sessionStorage.getItem('products-list'))
     
     const currentProductFullData = itemData.find(item => item.id === sessionStorage.getItem('current-product-id'))
     console.log(currentProductFullData);
+
+    document.querySelector('.indprod-name').textContent = currentProductFullData.name
+    document.querySelector('.indprod-category').textContent = currentProductFullData.category
+    document.querySelector('.indprod-price').textContent =currencyFormatter.format(currentProductFullData.priceInCents / 100)  
+    document.querySelector('.indprod-description').textContent = currentProductFullData.description
+    document.querySelector('.indprod-image-preview img').setAttribute('src', `../img/img-large/${currentProductFullData.image}`)
 }
