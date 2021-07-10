4d07b020e8b886cab2147526d78f6973f58a46f7
lkp25 <lkp25@o2.pl>
2021-07-10 10:32:12 +0200

    buy btn functinality basic in indiv prod js file

--- public/js/individual-product-page.js
+++ public/js/individual-product-page.js
@@ -1,30 +1,32 @@
+const buyBtn = document.querySelector('.indprod-buynow-btn')
 
-document.addEventListener('click', (e)=>{
-    if(e.target.classList.contains('product-card')){
-        const prodId = e.target.dataset.id
-        
-        openNewProductPage(prodId)
-    }
-    
-})
+populateTemplateWithData()
 
-async function openNewProductPage(prodId){
-    const response = await fetch(`http://localhost:5000/display-products/:${prodId}`)
-    console.log(response);
-    
-    window.location.href = response.url
-    sessionStorage.setItem('current-product-id', prodId)
-}
-getItemData()
 function getItemData(){
     const itemData = JSON.parse(sessionStorage.getItem('products-list'))
     
     const currentProductFullData = itemData.find(item => item.id === sessionStorage.getItem('current-product-id'))
-    console.log(currentProductFullData);
+    return currentProductFullData
 
+    
+}
+function populateTemplateWithData(){
+    const currentProductFullData = getItemData()
     document.querySelector('.indprod-name').textContent = currentProductFullData.name
     document.querySelector('.indprod-category').textContent = currentProductFullData.category
     document.querySelector('.indprod-price').textContent =currencyFormatter.format(currentProductFullData.priceInCents / 100)  
     document.querySelector('.indprod-description').textContent = currentProductFullData.description
     document.querySelector('.indprod-image-preview img').setAttribute('src', `../img/img-large/${currentProductFullData.image}`)
+}
+
+document.addEventListener('click' , e=>{
+    if(e.target === buyBtn){
+        checkIfItemIsInCart()
+    }
+})
+function checkIfItemIsInCart(){
+    const alreadyThere = itemsInCart.find(item=> item.id = getItemData().id)
+    if(alreadyThere){
+        
+    }
 }
