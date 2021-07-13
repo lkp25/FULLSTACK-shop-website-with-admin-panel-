e635f269a8551fa8ac85cfc965c14a35c29e41d8
lkp25 <lkp25@o2.pl>
2021-07-13 12:35:01 +0200

    added login functionality: bcrypt compare function checking for password match with the database and saving user data on the session object in case of success

--- routes/auth/login.js
+++ routes/auth/login.js
@@ -6,17 +6,46 @@ const router = express.Router()
 const rootDir = require('../../util/path')
 require('dotenv').config()
 
+const User = require('../../models/User-model')
+const mongoDB = require('../../util/mongoDBconnect').getDB
+const bcrypt = require('bcrypt')
+
+
 router.get('/login', (req, res, next) =>{
   res.sendFile(path.join(rootDir, 'views', 'login.html'))
   console.log(req.session.isLoggedIn);
 })
 
-router.post('/login', (req, res, next) =>{
-  req.session.isLoggedIn = true
-  req.session.save(()=>{
+router.post('/login', async (req, res, next) =>{
+  const email = req.body.email
+  const password = req.body.password
+  const getMongoDB = mongoDB()
 
-    res.redirect('/index')
-  })
+  const user = await getMongoDB.collection('users').findOne({email: email})
+  .then((user)=>{
+    if(!user){
+      res.redirect('/login')
+    }
+    bcrypt.compare(password, user.password) //makes it to then block EVEN if passwords dont match!!
+    .then((doMatch)=>{
+      if(doMatch){
+        req.session.isLoggedIn = true
+        req.session.user = user
+        //passwords match - return with saving session object
+        return req.session.save(()=>{
+          res.redirect('/index')
+
+        })
+      }
+      res.redirect('/login')
+      
+    })
+    .catch(err => {
+      console.log(err)
+      res.redirect('/login')
+
+    })
+  })  
  
 })
 
