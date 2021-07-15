e9386e954824de307d66d30301a4185eedede509
lkp25 <lkp25@o2.pl>
2021-07-15 09:52:56 +0200

    configurred nodemailer to send emails via app.js to my acount

--- app.js
+++ app.js
@@ -6,6 +6,9 @@ const app = express()
 
 const flash = require('connect-flash')
 const helmet = require('helmet')
+const nodemailer = require('nodemailer')
+const handlebars = require('express-handlebars')
+
 
 
 //setup for managing sessions and storing them in mongoDB
@@ -81,6 +84,36 @@ app.use('/', (req, res, next) =>{
     console.log('no such address');
     res.status(404).sendFile(path.join(rootDir, 'views', '404.html'))
 })
+async function main() {
+ 
+
+let transporter = nodemailer.createTransport({
+    host: "poczta.o2.pl",
+    port: 465,
+    secure: true, // true for 465, false for other ports
+    auth: {
+      user: process.env.MYMAIL_LOGIN, 
+      pass: process.env.MYMAIL_PASS
+    },
+  });
+
+  // send mail with defined transport object
+  let info = await transporter.sendMail({
+    from: '"LKPSHOP" <lkp25@o2.pl>', // sender address
+    to: "lkp25@o2.pl", // list of receivers
+    subject: "Hello ✔", // Subject line
+    text: "Hello world?", // plain text body
+    html: "<b>Hello world?</b>", // html body
+  });
+  console.log("Message sent: %s", info.messageId);
+  // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
+
+  // Preview only available when sending through an Ethereal account
+  console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
+  // Preview URL: https://ethereal.email/message/WaQKMgKddxQDoou...
+}
+
+main().catch(console.error);
 
 //start server
 app.listen(5000)
